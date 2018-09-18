0bde2c951edf4562269c5d78eecd396a9dab6756
Zaldy <zframeworkdev@gmail.com>
2018-09-18 12:42:21 +0800

    Our first commit

---
+++ Test01Kit/Test01Kit/HahahaManager.swift
@@ -0,0 +1,16 @@
+//
+//  HahahaManager.swift
+//  Test01Kit
+//
+//  Created by Zaldy on 18/09/2018.
+//  Copyright © 2018 Zaldy. All rights reserved.
+//
+
+import Foundation
+
+public class HaHaHaManager {
+    public init() { }
+    public func awesomeFunction(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
---
+++ Test01Kit/Test01Kit/Test01Kit.h
@@ -0,0 +1,19 @@
+//
+//  Test01Kit.h
+//  Test01Kit
+//
+//  Created by Zaldy on 18/09/2018.
+//  Copyright © 2018 Zaldy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Test01Kit.
+FOUNDATION_EXPORT double Test01KitVersionNumber;
+
+//! Project version string for Test01Kit.
+FOUNDATION_EXPORT const unsigned char Test01KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Test01Kit/PublicHeader.h>
+
+
